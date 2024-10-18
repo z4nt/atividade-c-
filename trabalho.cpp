912315e6cbae2f298707a1f2a9e9fe80918ddaed
Antoniel <antonielf1203@gmail.com>
2024-10-18 19:14:36 -0300

    finalizado

--- trabalho.cpp
+++ trabalho.cpp
@@ -33,9 +33,10 @@ public:
             tail->next = newNode;
             tail = newNode;
         }
+        cout << "O elemento inserido foi: " << data << endl;
     }
 
-    void remove(){
+    void removeUm(){
         if(head == nullptr) return;
             Node* temp = head;
             head = head->next;
@@ -43,9 +44,54 @@ public:
             delete temp;
             return;
     }
+
+    void removeVarios(int tamanho){
+        if(head == nullptr) return;
+        for (int i = 0; i < tamanho; i++)
+        {
+            removeUm();
+        }
+        
+    }
+
+    void print()
+    {
+        Node* temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Digite o tamanho da lista: ";
+    int n;
+    cin>>n;
+    List list;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        list.insert(x);
+    }
+    cout << "Digite a quantidade de elementos que deseja remover: ";
+    int m;
+    cin>>m;
+    if (m <= 0 )
+    {
+        cout << "Nada foi removido" << endl;
+    }else if (m > n)
+    {
+        cout << "A quantidade de elementos que deseja remover é maior que a quantidade de elementos na lista" << endl;
+    }else if (m == 1)
+    {
+        list.removeUm();
+    }else
+    {
+        list.removeVarios(m);
+    }
+    list.print();
     return 0;
 }
